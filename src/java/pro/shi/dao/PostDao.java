c22424457f34edcb9cda7a821b58f0d65a0218db
Shivansh Tariyal <shivanshtariyal@gmail.com>
2024-10-30 10:31:51 +0530

    fixed

--- src/java/pro/shi/dao/PostDao.java
+++ src/java/pro/shi/dao/PostDao.java
@@ -24,7 +24,7 @@ public class PostDao {
     try {
         String q = "select * from categories";
         Statement st = this.con.createStatement();
-        ResultSet set = st.executeQuery(q);
+        ResultSet set = st	.executeQuery(q);
         while (set.next()) {
             int cid = set.getInt("cid");
             String name = set.getString("name");
